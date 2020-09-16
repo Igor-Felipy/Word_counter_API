3be3c54ae7b57a226914b0cad76e82e7dbbc4f7a
Igor-Felipy <igor.felipy123@gmail.com>
2020-09-15 23:01:36 -0400

    api retornando os dados corretamente

--- app/controllers/words.py
+++ app/controllers/words.py
@@ -1,5 +1,25 @@
 from . import controllers
+from flask import request
 
-@controllers.route("/")
-def main():
-    return "main"
+
+@controllers.route("/", methods=["POST"])
+def answer():
+    text_old = request.json
+    text = text_old["text"]
+    text_cleared = clean_text(text)
+    count_dict = counter(text_cleared)
+    return count_dict
+
+
+
+def clean_text(text):
+    pontuation = [".",",",":","'",'"',"{","}","[","]","/",";","-","(",")","_","*","?","+",">","<","=","%","$","@","!","&","#"]
+    new_text = "".join(c for c in text if c not in pontuation).upper()
+    list = new_text.split()
+    return list
+
+def counter(list):
+    count = dict()
+    for n in list:
+        count[n] = count.get(n,0)+1
+    return count
